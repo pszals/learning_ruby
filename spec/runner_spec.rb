304017733845d126075779cbb6ad4fe466aaa436
Philip Szalwinski <pszalwinski@gmail.com>
2013-05-22 20:22:42 -0400

    Added bash script to run file, added spec for Runner. Game can be played and tests can be run from parent directory

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -6,11 +6,7 @@ require 'game'
 describe Runner do
   it "calls the play_game method with board, io, and game classes to start the game" do
     runner = Runner.new
-    board = Board.new
-    io = Io.new
-    game = Game.new(board, io)
-    game.should_receive(:play_game)
-    io.should_receive(:get_input)
+    runner.should_receive(:start_game)
     runner.start_game
   end  
 end
