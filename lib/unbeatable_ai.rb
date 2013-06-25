28e7ab924dfa876675c978bd641b746fbddf8306
Philip Szalwinski <pszalwinski@gmail.com>
2013-06-25 14:03:16 -0500

    Renamed and refactored unbeatable ai class to prepare for minimax algorithm

---
+++ lib/unbeatable_ai.rb
@@ -0,0 +1,41 @@
+require 'board'
+
+class UnbeatableAI
+  
+  attr_accessor :board
+  
+  def initialize
+    @board = Board.new
+  end
+
+  def score_board(board)
+    if !board.board_open? and !board.winner_on_board?
+      return 0 
+    elsif board.winner_on_board? == 'X'
+      return 1
+    else
+      return -1
+    end
+  end
+
+end
+
+
+=begin
+  
+    IF it is marker's turn
+      For each empty square on board, set_square(square, marker)
+      IF winner_on_board == True
+        square_score += 1
+        undo_set_square(square)
+
+      ELSE 
+        put a O in that square
+        undo_set_square(square)
+    
+    IF it is O's turn and Find winner on board == True
+      square_score -= 1
+    ELSIF it is X's turn and Find winner on board == True
+      square_score += 1
+      
+=end
---
+++ spec/unbeatable_ai_spec.rb
@@ -0,0 +1,36 @@
+require 'unbeatable_ai'
+require 'board'
+
+describe UnbeatableAI do
+  it "returns 1 if X wins" do
+    ai = UnbeatableAI.new
+    board = Board.new
+    board.current_board = ['X', 'X', 'X', 
+                           '4', '5', '6', 
+                           '7', '8', '9']
+    ai.score_board(board).should == 1
+  end
+  
+  it "returns -1 if O wins" do
+    ai = UnbeatableAI.new
+    board = Board.new
+    board.current_board = ['O', 'O', 'O', 
+                           '4', '5', '6', 
+                           '7', '8', '9']
+    ai.score_board(board).should == -1      
+  end
+  
+  it "returns 0 if game is tied" do
+    ai = UnbeatableAI.new
+    board = Board.new
+    board.current_board = ['O', 'O', 'X', 
+                           'X', 'O', 'O', 
+                           'O', 'X', 'X']
+    ai.score_board(board).should == 0
+  end
+  
+  it "returns best index" do
+    
+  end
+  
+end
