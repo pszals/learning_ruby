232ea0715f2d0bb2a157c7a71541228f9204e95d
Philip Szalwinski <pszalwinski@gmail.com>
2013-07-09 09:58:44 -0500

    Removed extraneous test setup

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -6,14 +6,7 @@ require 'configuration'
 
 describe Runner do
 
-  let(:player_1)      { Player.new('X')                             }
-  let(:player_2)      { Player.new('O')                             }
-  let(:board)         { Board.new(player_1, player_2)               }
-  let(:io)            { mock.as_null_object                         }
-  let(:ai)            { Unbeatable_AI.new                           }
-  let(:game)          { Game.new(board, io, ai, player_1, player_2) }
-  let(:configuration) { Configuration.new                           }
-  let(:runner)        { Runner.new                                  }
+  let(:runner) { Runner.new }
 
   it "starts up the game" do
     runner.should_receive(:start_game)
